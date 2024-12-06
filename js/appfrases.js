a988a892dde642d31e1da1d825498e232198ffac
EPGualoto <epgualoto@uce.edu.ec>
2024-12-06 00:37:34 +0000

    taller 15

--- js/appfrases.js
+++ js/appfrases.js
@@ -9,12 +9,62 @@ const frases = [{ frase: 'Al que madruga Dios le ayuda', autor: 'Pepito Perez' }
 
 const app = Vue.createApp({
     methods: {
+        agregarFrase() {
+            console.log(this.frase);
+            console.log(this.autor);
+
+            const nuevaFrase = {
+                frase: this.frase,
+                autor: this.autor
+            }
+
+            this.listafrases.unshift(nuevaFrase);
+        },
+
+        agregarFraseFinal() {
+            console.log(this.frase);
+            console.log(this.autor);
+
+            const nuevaFrase = {
+                frase: this.frase,
+                autor: this.autor
+            }
+
+            this.listafrases.push(nuevaFrase);
+        },
+
+        eventoKeyPress({ charCode, cancelable, key, keyCode, target }) {
+            if (key === 'Enter') {
+                console.log('Evento');
+                //console.log(event);
+                console.log(key);
+                console.log(charCode);
+                console.log(keyCode);
+                console.log(cancelable);
+                console.log(target.baseURI);
+                this.agregarFraseFinal();
+            }
+        },
+
+        eventoKeyPressModificador() {
+            console.log('Evento');
+            console.log(key);
+            console.log(charCode);
+            console.log(keyCode);
+            console.log(cancelable);
+            console.log(target.baseURI);
+            //console.log(event);
+            this.agregarFraseFinal();
+
+        }
 
     },
     data() {
         return {
-            listafrases: frases
-
+            listafrases: frases,
+            frase: null,
+            //autor: 'Sin autor'
+            autor: null
         }
     }
 })
